5b0377baeca888f53ccc778e8460b2d9203d8acc
Vinay <v@v.com>
2018-01-03 18:02:35 +0000

    Test Updates

--- src/test/java/in/erail/tutorial/SessionTest.java
+++ src/test/java/in/erail/tutorial/SessionTest.java
@@ -3,6 +3,7 @@ package in.erail.tutorial;
 import in.erail.glue.Glue;
 import in.erail.server.Server;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -22,7 +23,32 @@ public class SessionTest {
   public Timeout rule = Timeout.seconds(2000);
 
   @Test
-  public void testAGetRequest(TestContext context) {
+  public void testAPostRequest(TestContext context) {
+
+    Async async = context.async(5);
+
+    Server server = Glue.instance().<Server>resolve("/in/erail/server/Server");
+    String content = new JsonObject().put("session", "random").toString();
+
+    for (int i = 0; i < 5; ++i) {
+      server
+              .getVertx()
+              .createHttpClient()
+              .post(server.getPort(), server.getHost(), "/v1/session")
+              .putHeader("content-type", "application/json")
+              .putHeader("content-length", Integer.toString(content.length()))
+              .handler(response -> {
+                context.assertEquals(response.statusCode(), 200, response.statusMessage());
+                async.countDown();
+              })
+              .write(content)
+              .end();
+    }
+
+  }
+
+  @Test
+  public void testBGetRequest(TestContext context) {
 
     Async async = context.async(2);
     Server server = Glue.instance().<Server>resolve("/in/erail/server/Server");
