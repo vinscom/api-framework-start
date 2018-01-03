7a9ba62b071c3c89ee71fefe8a3acc633063979a
Vinay <v@v.com>
2018-01-03 17:33:03 +0000

    Update test Cases

--- src/main/java/in/erail/tutorial/SessionGetServiceV2.java
+++ src/main/java/in/erail/tutorial/SessionGetServiceV2.java
@@ -2,8 +2,7 @@ package in.erail.tutorial;
 
 import io.vertx.core.json.JsonArray;
 import io.vertx.reactivex.core.shareddata.LocalMap;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  *
@@ -13,11 +12,11 @@ public class SessionGetServiceV2 extends SessionGetService {
 
   @Override
   public JsonArray getSessions() {
-    LocalMap<String, List<String>> data = getVertx().sharedData().<String, List<String>>getLocalMap("data");
-    List<String> sessions = data.getOrDefault("sessions", new ArrayList<>());
+    LocalMap<String, String> data = getVertx().sharedData().<String, String>getLocalMap("data");
+    String sessions = data.getOrDefault("sessions", "");
 
     JsonArray result = new JsonArray();
-    sessions.forEach((s) -> {
+    Arrays.stream(sessions.split(",")).forEach((s) -> {
       result.add(s);
     });
 
