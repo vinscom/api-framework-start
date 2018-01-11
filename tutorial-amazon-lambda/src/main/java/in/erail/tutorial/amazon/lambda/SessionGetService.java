816e4a43b0900cc1b3f0195a719493593ee0600a
Vinay <v@v.com>
2018-01-11 01:50:59 +0000

    Amazon lambda project working

---
+++ tutorial-amazon-lambda/src/main/java/in/erail/tutorial/amazon/lambda/SessionGetService.java
@@ -0,0 +1,11 @@
+package in.erail.tutorial.amazon.lambda;
+
+import in.erail.amazon.lambda.LambdaInvokerHelper;
+
+public class SessionGetService extends LambdaInvokerHelper {
+
+  public SessionGetService() {
+    super("/in/erail/tutorial/SessionGetService");
+  }
+
+}
