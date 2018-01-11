6c876a6fa19b13db59ae9f37ba4fab698df5807a
Vinay <v@v.com>
2018-01-11 19:59:05 +0000

    Update document

--- tutorial-amazon-lambda/src/main/java/in/erail/tutorial/amazon/lambda/SessionGetService.java
+++ tutorial-amazon-lambda/src/main/java/in/erail/tutorial/amazon/lambda/SessionGetService.java
@@ -1,8 +1,8 @@
 package in.erail.tutorial.amazon.lambda;
 
-import in.erail.amazon.lambda.LambdaInvokerHelper;
+import in.erail.amazon.lambda.AWSLambda;
 
-public class SessionGetService extends LambdaInvokerHelper {
+public class SessionGetService extends AWSLambda {
 
   public SessionGetService() {
     super("/in/erail/tutorial/SessionGetService");
